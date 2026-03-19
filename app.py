bad18773af982cf363e72ed10c5efed6381f772c
jjzfi <amjed7988@gmail.com>
2026-03-20 02:29:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 # ==========================================
 # 📥 تحميل المودل من Google Drive (تلقائي)
 # ==========================================
-MODEL_PATH = "weights.h5"
+MODEL_PATH = "model.h5"
 
 if not os.path.exists(MODEL_PATH):
     print("Downloading model from Google Drive...")
